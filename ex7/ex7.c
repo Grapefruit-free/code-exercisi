f86bfbf698f73d3bd667349089318c64a2fdd047
Grape_glb <grapefruit_freedom_glb@163.com>
2020-01-10 22:23:34 +0800

    自己创建函数，用单独文件保存(xxxx.h)，程序是交换两个浮点数.

---
+++ ex7/ex7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"max.h"
+
+int main(void){
+	double a,b;
+	scanf("%lf",&a);
+	scanf("%lf",&b);
+	
+	printf("%lf",max(a,b));
+	return EXIT_SUCCESS;
+}
---
+++ ex7/max.h
@@ -0,0 +1,6 @@
+double max(double n,double m){
+      if(n>m)
+	     return n;
+	  else
+         return m;
+}		 
